2029a2a9980b93d0e4fedc505a77a3b2a3be9e03
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-18 19:57:30 +0100

    added node external and run only in production

--- applications/server/rolldown.config.ts
+++ applications/server/rolldown.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "rolldown";
 import run from "@rollup/plugin-run";
+import nodeExternals from "rollup-plugin-node-externals";
+
+const isProduction = process.env["PRODUCTION"] ? true : false
 
 export default defineConfig({
   input: "index.ts",
   plugins: [
-    run(),
+    nodeExternals(),
+    isProduction && run(),
   ],
   output: {
     file: "dist/index.js",
