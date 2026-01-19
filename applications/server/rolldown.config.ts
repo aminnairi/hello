8f3d9a78b064615c0dad6ac946d8684af6ea2b38
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-19 12:28:19 +0100

    using the rollup env instead of the hardcoded one

--- applications/server/rolldown.config.ts
+++ applications/server/rolldown.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "rolldown";
 import run from "@rollup/plugin-run";
 import nodeExternals from "rollup-plugin-node-externals";
 
-const isProduction = process.env["PRODUCTION"] ? true : false
+const isDevelopment = !!process.env["ROLLUP_WATCH"];
+
+console.log(process.env);
 
 export default defineConfig({
   input: "index.ts",
   plugins: [
     nodeExternals(),
-    isProduction && run(),
+    isDevelopment && run(),
   ],
   output: {
     file: "dist/index.js",
