6f71ab61c0533b2e24084782d5b1ebffd0fdb1bb
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-19 17:52:30 +0100

    removed unecessary log

--- applications/server/rolldown.config.ts
+++ applications/server/rolldown.config.ts
@@ -4,8 +4,6 @@ import nodeExternals from "rollup-plugin-node-externals";
 
 const isDevelopment = !!process.env["ROLLUP_WATCH"];
 
-console.log(process.env);
-
 export default defineConfig({
   input: "index.ts",
   plugins: [
