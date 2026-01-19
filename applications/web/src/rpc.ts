83e38ff968c3b8234a5fc702b943d95e536cb9e5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-19 12:28:37 +0100

    removed the separated rpc definition

--- applications/web/src/rpc.ts
+++
@@ -1,7 +0,0 @@
-import { createHTTPRequest } from "@aminnairi/rpc-web";
-import { routes } from "@hello/server/routes";
-
-export const request = createHTTPRequest({
-  routes,
-  url: "http://localhost:8000"
-});
