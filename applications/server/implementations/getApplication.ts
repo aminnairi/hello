ab7c13c5516b82ece54d08c804863b902538e957
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-21 13:59:01 +0100

    now returning an identifier for the apps

--- applications/server/implementations/getApplication.ts
+++ applications/server/implementations/getApplication.ts
@@ -1,8 +1,15 @@
+import { randomUUID } from "crypto";
 import { implementGetApplications } from "../routes/getApplications";
 import { settings } from "../settings"
 
 export const getApplications = implementGetApplications(async () => {
   return {
-    applications: settings.applications,
+    applications: settings.applications.map(application => {
+      return {
+        identifier: randomUUID(),
+        name: application.name,
+        url: application.url,
+      }
+    }),
   };
 });
