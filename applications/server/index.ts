00b56d5f964ecd39169ad52c425cf496e9cc6939
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-21 13:43:57 +0100

    updated the message sent when starting the http server

--- applications/server/index.ts
+++ applications/server/index.ts
@@ -14,5 +14,5 @@ const server = createHTTPServer({
 });
 
 server.listen(8000, "0.0.0.0", () => {
-  console.log(`[HELLO-SERVER] HTTP server listening on http://0.0.0.0:8000`);
+  console.log(`HTTP server listening on http://0.0.0.0:8000`);
 });
