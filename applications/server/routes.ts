47f4efc29c80491a71cacab0c4439c0a081c7b4d
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-21 22:30:07 +0100

    added a new sign in route

--- applications/server/routes.ts
+++ applications/server/routes.ts
@@ -2,9 +2,11 @@ import { createHTTPRoutes } from "@aminnairi/rpc";
 import { getCryptos } from "./routes/getCryptos";
 import { getApplications } from "./routes/getApplications";
 import { getWeather } from "./routes/getWeather";
+import { signIn } from "./routes/signIn";
 
 export const routes = createHTTPRoutes({
   getApplications,
   getCryptos,
   getWeather,
+  signIn,
 });
