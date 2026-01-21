959d718b4d34f9b29ba0d18d5a5962b558b80cff
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-21 13:48:00 +0100

    exported the routes in their own files

--- applications/server/routes.ts
+++ applications/server/routes.ts
@@ -1,53 +1,7 @@
-import { createHTTPRoute, createHTTPRoutes } from "@aminnairi/rpc";
-import { z } from "zod";
-import { applicationsSchema, cryptosSchema } from "./schema";
-
-export const [getApplications, implementGetApplications] = createHTTPRoute({
-  parseInput: input => {
-    return z.null().parse(input);
-  },
-  parseOutput: output => {
-    return z.object({
-      applications: applicationsSchema,
-    }).parse(output);
-  }
-});
-
-export const [getCryptos, implementGetCryptos] = createHTTPRoute({
-  parseInput: input => {
-    return z.null().parse(input);
-  },
-  parseOutput: output => {
-    return cryptosSchema.parse(output);
-  }
-});
-
-export const weatherSchema = z.object({
-  main: z.string(),
-  description: z.string(),
-  temperature: z.number(),
-  humidity: z.number(),
-  pressure: z.number(),
-});
-
-export type Weather = z.infer<typeof weatherSchema>;
-
-export const [getWeather, implementGetWeather] = createHTTPRoute({
-  parseInput: input => {
-    return z.null().parse(input);
-  },
-  parseOutput: output => {
-    return z.union([
-      z.object({
-        success: z.literal(false),
-      }),
-      z.object({
-        success: z.literal(true),
-        weather: weatherSchema,
-      }),
-    ]).parse(output);
-  }
-});
+import { createHTTPRoutes } from "@aminnairi/rpc";
+import { getCryptos } from "./routes/getCryptos";
+import { getApplications } from "./routes/getApplications";
+import { getWeather } from "./routes/getWeather";
 
 export const routes = createHTTPRoutes({
   getApplications,
