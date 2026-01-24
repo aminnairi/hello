b5d70b01ac53d10e8558b4cc86b1dbe9ebe07385
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-24 14:22:51 +0100

    updated the name of the top-level properties in the settings.json configuration schema

--- applications/server/schema.ts
+++ applications/server/schema.ts
@@ -11,12 +11,12 @@ export const applicationsSettingsSchema = z.array(z.object({
   url: z.url({ error: "it should be a valid URL" }),
 }, { error: "it should be an object" }), { error: "it should be an array" });
 
-export const settingsCryptoSchema = z.array(z.object({
+export const cryptoSettingSchema = z.array(z.object({
   name: z.string({ error: "it should be a string" }),
   ticker: z.string({ error: "it should be a string" }),
 }, { error: "it should be an object" }), { error: "it should be an array" });
 
-export const openweathermapSettingsSchema = z.object({
+export const weatherSettingSchema = z.object({
   apiKey: z.string({ error: "it should be a string" }),
   city: z.string({ error: "it should be a string" }),
   language: z.string({ error: "it should be a string" }),
@@ -25,8 +25,8 @@ export const openweathermapSettingsSchema = z.object({
 export const settingsSchema = z.object({
   authentication: authenticationSettingSchema,
   applications: applicationsSettingsSchema,
-  crypto: settingsCryptoSchema,
-  openweathermap: openweathermapSettingsSchema,
+  cryptos: cryptoSettingSchema,
+  weather: weatherSettingSchema,
 }, { error: "it should be an object" });
 
 export type Applications = z.infer<typeof applicationsSettingsSchema>;
