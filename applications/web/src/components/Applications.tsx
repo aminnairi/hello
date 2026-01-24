5072560931f50623332198a88b3c1dce977dccaa
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-24 13:02:54 +0100

    new design for apps and icon buttons for actions such as copy, share and open

--- applications/web/src/components/Applications.tsx
+++ applications/web/src/components/Applications.tsx
@@ -1,27 +1,63 @@
-import { OpenInNew, Public } from "@mui/icons-material";
-import { Button, Card, CardActions, CardContent, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Skeleton, Stack, Typography, Zoom } from "@mui/material";
+import { ContentCopy, OpenInNew, Share } from "@mui/icons-material";
+import { Button, Card, CardActions, CardContent, CardHeader, IconButton, Link, Skeleton, Stack, Tooltip, Typography, Zoom } from "@mui/material";
 import { useApplications } from "../hooks/useApplications";
-import { useCallback, useEffect } from "react";
+import { Fragment, useCallback, useEffect, useMemo } from "react";
+import { useNotification } from "../hooks/useNotification";
 
 export const Applications = () => {
   const { filteredApplications, loadingApplications, getApplications } = useApplications();
+  const { openSuccessNotification, openErrorNotification } = useNotification();
 
   useEffect(() => {
     getApplications();
   }, [getApplications]);
 
+  const canShare = useMemo(() => {
+    return typeof window.navigator.share === "function";
+  }, []);
+
+  const canCopy = useMemo(() => {
+    return typeof window.navigator.clipboard === "object"
+      && typeof window.navigator.clipboard.writeText === "function";
+  }, []);
+
   const onApplicationListItemButtonClicked = useCallback((url: string) => () => {
     window.open(url);
   }, []);
 
+  const onCopyIconButtonClick = useCallback((url: string) => () => {
+    navigator.clipboard.writeText(url).then(() => {
+      openSuccessNotification("URL copied to clipboard!");
+    }).catch(error => {
+      openErrorNotification("Failed to copy to clipboard");
+      console.error(error);
+    });
+  }, [openErrorNotification, openSuccessNotification]);
+
+  const onShareIconButtonClick = useCallback((title: string, url: string) => () => {
+    navigator.share({
+      title,
+      url,
+      text: `Navigate to ${title}: ${url}`,
+    }).then(() => {
+      openSuccessNotification("Successfully shared link!");
+    }).catch(error => {
+      openErrorNotification("Failed to share link");
+      console.error(error);
+    });
+  }, [openErrorNotification, openSuccessNotification]);
+
   return (
-    <Card>
-      <CardContent>
-        <Zoom appear in={true}>
-          <List>
-            {loadingApplications ? (
-              <Stack spacing={3} paddingBottom={3}>
-                {Array.from(Array(3)).map((_, index) => (
+    <Fragment>
+      <Typography variant="h6">
+        Applications
+      </Typography>
+      <Zoom appear in={true}>
+        {loadingApplications ? (
+          <Stack spacing={3} paddingBottom={3}>
+            {Array.from(Array(3)).map((_, index) => (
+              <Card>
+                <CardContent>
                   <Stack direction="row" minWidth="300px" spacing={3} alignItems="center" key={index}>
                     <Skeleton variant="circular" width="40px" height="40px" />
                     <Stack flex="1">
@@ -30,34 +66,47 @@ export const Applications = () => {
                     </Stack>
                     <Skeleton variant="rectangular" height="40px" width="40px" />
                   </Stack>
-                ))}
-              </Stack>
-            ) : (
-              <Zoom appear in={true}>
-                <Stack>
-                  {filteredApplications.length === 0 ? (
-                    <Typography align="center">
-                      No matching applications.
-                    </Typography>
-                  ) : filteredApplications.map((application) => (
-                    <ListItem key={application.identifier}>
-                      <ListItemButton onClick={onApplicationListItemButtonClicked(application.url)}>
-                        <ListItemIcon>
-                          <Public />
-                        </ListItemIcon>
-                        <ListItemText primary={application.name} secondary={new URL(application.url).host} />
-                        <ListItemIcon sx={{ paddingLeft: 3 }}>
-                          <OpenInNew />
-                        </ListItemIcon>
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </Stack>
-              </Zoom>
-            )}
-          </List>
-        </Zoom>
-      </CardContent>
+                </CardContent>
+              </Card>
+            ))}
+          </Stack>
+        ) : (
+          <Zoom appear in={true}>
+            <Stack spacing={3}>
+              {filteredApplications.length === 0 ? (
+                <Typography align="center">
+                  No matching applications.
+                </Typography>
+              ) : filteredApplications.map((application) => (
+                <Card key={application.identifier}>
+                  <CardHeader title={application.name} subheader={new URL(application.url).host} />
+                  <CardActions sx={{ justifyContent: "right" }}>
+                    {canShare && (
+                      <Tooltip title="Share URL">
+                        <IconButton onClick={onShareIconButtonClick(application.name, application.url)}>
+                          <Share />
+                        </IconButton>
+                      </Tooltip>
+                    )}
+                    {canCopy && (
+                      <Tooltip title="Copy URL">
+                        <IconButton onClick={onCopyIconButtonClick(application.url)}>
+                          <ContentCopy />
+                        </IconButton>
+                      </Tooltip>
+                    )}
+                    <Tooltip title="Open">
+                      <IconButton onClick={onApplicationListItemButtonClicked(application.url)}>
+                        <OpenInNew />
+                      </IconButton>
+                    </Tooltip>
+                  </CardActions>
+                </Card>
+              ))}
+            </Stack>
+          </Zoom>
+        )}
+      </Zoom>
       {!loadingApplications && (
         <CardActions>
           <Button component={Link} href="https://selfh.st/apps/" target="blank">
@@ -65,6 +114,6 @@ export const Applications = () => {
           </Button>
         </CardActions>
       )}
-    </Card>
+    </Fragment>
   );
 };
