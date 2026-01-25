7b9f080079087883357e5f95a71c8ea22f3aa48b
aminnairi <18418459+aminnairi@users.noreply.github.com>
2026-01-25 18:29:25 +0100

    initial commit

---
+++ applications/web/src/hooks/useAuthentication.ts
@@ -0,0 +1,23 @@
+import { useCallback } from "react";
+import { useToken } from "./useToken";
+import { useApplications } from "./useApplications";
+import { useCryptos } from "./useCryptos";
+import { useWeather } from "./useWeather";
+
+export const useAuthentication = () => {
+  const { clearToken } = useToken();
+  const { clearApplications } = useApplications();
+  const { clearCryptos } = useCryptos();
+  const { clearWeather } = useWeather();
+
+  const logout = useCallback(() => {
+    clearToken();
+    clearApplications();
+    clearCryptos();
+    clearWeather();
+  }, [clearApplications, clearCryptos, clearToken, clearWeather]);
+
+  return {
+    logout,
+  };
+};
